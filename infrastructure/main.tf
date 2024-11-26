4f0dbd9d5ce6f94d73e8b1a87087237d6001c650
Antoine Maes <92756820+antoine-maes@users.noreply.github.com>
2024-11-26 17:44:03 +0100

    feat: create postgres instance on azure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,13 @@
-resource "azurerm_resource_group" "projet-final" {
+resource "azurerm_resource_group" "rg" {
   name     = "projet-final-rg"
   location = var.rg_location
+}
+
+
+module "postgresdb" {
+  source                    = "./postgresdb"
+  location                  = azurerm_resource_group.rg.location
+  rg_name                   = azurerm_resource_group.rg.name
+  pg_admin_username         = var.pg_admin_username
+  pg_admin_password         = var.pg_admin_password
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,3 +3,15 @@ variable "rg_location" {
   default     = "francecentral"
   description = "Location of the resource group"
 }
+
+
+variable "pg_admin_username" {
+  type        = string
+  description = "Username for the PostgreSQL admin"
+}
+
+variable "pg_admin_password" {
+  type        = string
+  description = "Password for the PostgreSQL admin"
+  sensitive = true
+}
