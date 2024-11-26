c813e6dab84a8cabb7e4da61ab497dd1faa04530
Antoine Maes <92756820+antoine-maes@users.noreply.github.com>
2024-11-26 16:51:52 +0100

    feat(infra)!: setup terraform & azure ressource group

---
+++ infrastructure/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "projet-final" {
+  name     = "projet-final-rg"
+  location = var.rg_location
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,5 @@
+variable "rg_location" {
+  type        = string
+  default     = "francecentral"
+  description = "Location of the resource group"
+}
