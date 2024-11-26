92e777cd89ff0dc7169aa904d538772e5385c22d
Antoine Maes <92756820+antoine-maes@users.noreply.github.com>
2024-11-26 18:56:34 +0100

    fix(infra): change location to a broader one

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,9 +1,8 @@
 variable "rg_location" {
   type        = string
-  default     = "francecentral"
+  default     = "northeurope"
   description = "Location of the resource group"
 }
-
 
 variable "pg_admin_username" {
   type        = string
