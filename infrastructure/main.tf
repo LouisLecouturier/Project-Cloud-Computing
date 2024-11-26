00f0002d46b603b645d51e491ec84645d646f57e
Antoine Maes <92756820+antoine-maes@users.noreply.github.com>
2024-11-26 18:57:09 +0100

    feat(infra): add API on terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,10 +4,16 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
-module "postgresdb" {
-  source                    = "./postgresdb"
+module "postgres-db" {
+  source                    = "./postgres-db"
   location                  = azurerm_resource_group.rg.location
   rg_name                   = azurerm_resource_group.rg.name
   pg_admin_username         = var.pg_admin_username
   pg_admin_password         = var.pg_admin_password
+}
+
+module "python-api" {
+  source                    = "./python-api"
+  location                  = azurerm_resource_group.rg.location
+  rg_name                   = azurerm_resource_group.rg.name
 }
