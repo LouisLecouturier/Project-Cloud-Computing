673342ff870fb990468395adbfea67198907321f
Nicolas Broage <nicolas.chailles@orange.fr>
2024-11-26 12:02:19 +0100

    init db

--- api/main.py
+++ api/main.py
@@ -9,5 +9,9 @@ app.include_router(router, prefix="/api")
 
 if __name__ == "__main__":
     import uvicorn
+    
+    # Drop all the db (don't use in prod, only for demo)
+    
+    # Seed db
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
