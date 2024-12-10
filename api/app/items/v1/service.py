0bce1df86ced8d3ba9aa031807de59f43799a985
Louis Lecouturier <71454059+LouisLecouturier@users.noreply.github.com>
2024-12-10 18:08:02 +0100

    fix(ci): triggered ci on purpose

--- api/app/items/v1/service.py
+++ api/app/items/v1/service.py
@@ -1,9 +1,10 @@
 from database.utils.db import DatabaseConnection
 
+
 class ItemService:
     def __init__(self):
         self.db_connection = DatabaseConnection()
-        
+
         print("ITEMS Service...")
 
     def get_item(self, id: int):
@@ -15,9 +16,9 @@ class ItemService:
                 "id": item[0],
                 "name": item[1],
                 "description": item[2],
-                "price": float(item[3])
+                "price": float(item[3]),
             }
-            
+
         return response_data
 
     def get_all_items(self):
@@ -25,12 +26,13 @@ class ItemService:
             cursor.execute("SELECT * FROM items")
             items = cursor.fetchall()
             response_data = [
-            {
-                "Id": item[0],
-                "Name": item[1],
-                "Description": item[2],
-                "Price": item[3]
-            } for item in items
-        ]
-        
+                {
+                    "Id": item[0],
+                    "Name": item[1],
+                    "Description": item[2],
+                    "Price": item[3],
+                }
+                for item in items
+            ]
+
         return {"items": response_data}
